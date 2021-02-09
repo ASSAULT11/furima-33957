b7639e39eddbb6c300c4857eefeaf324f0824736
ASSAULT11 <77090569+ASSAULT11@users.noreply.github.com>
2021-02-09 16:56:48 +0900

    userモデルにバリデーション＋外部キー記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :kana_last, presence: true
+  validates :kana_first, presence: true
+  validates :birthday, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
 end
