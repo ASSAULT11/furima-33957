e74ac569809daa4a97daec367d0b87656a32c415
ASSAULT11 <77090569+ASSAULT11@users.noreply.github.com>
2021-02-11 15:13:39 +0900

    furimaモデル作成＋migrate実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_08_080339) do
+ActiveRecord::Schema.define(version: 2021_02_11_060041) do
+
+  create_table "furimas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "item", null: false
+    t.text "text", null: false
+    t.integer "category_id", null: false
+    t.integer "state_id", null: false
+    t.integer "shipping_id", null: false
+    t.integer "area_id", null: false
+    t.integer "send_day_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_furimas_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2021_02_08_080339) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "furimas", "users"
 end
