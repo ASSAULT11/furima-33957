1a5623b7022f8fba759f7bf0a6f9ef10d518dd70
ASSAULT11 <77090569+ASSAULT11@users.noreply.github.com>
2021-02-11 14:57:48 +0900

    ルーティング、コントローラー、商品出品のビュー追加

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -1,4 +1,24 @@
 class FurimasController < ApplicationController
   def index
+    @furimas = Furima.all
+  end
+
+  def new
+    @furima = Furima.new
+  end
+
+  def create
+    @furima = Furima.new(prototype_params)
+    if @furima.valid?
+      @furima.save
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+  private
+  def tweet_params
+    params.require(:furima).permit(:item, :image, :text, :category_id, :state_id, :shipping_id, :area_id, :send_day_id, :price).merge(user_id: current_user.id)
   end
 end
