9f18736ac172bbd19b109dd162f20c200fd30c05
ASSAULT11 <77090569+ASSAULT11@users.noreply.github.com>
2021-02-12 18:19:14 +0900

    FactoryBot記述

--- spec/factories/furimas.rb
+++ spec/factories/furimas.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :furima do
-    
+    image             {app/assets/images/flag.png}
+    item              {'kkk'}
+    text              {'kkk'}
+    category_id       {'ベビー・キッズ'}
+    state_id          {'傷や汚れあり'}
+    shipping_id       {'送料込み(出品者負担)'}
+    area_id           {'千葉県'}
+    send_day_id       {'1〜2日で発送'}
+    price             {'1000'}
   end
 end
