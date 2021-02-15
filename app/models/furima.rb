e40b439f32e0f07b799fb07fc3fd8e2708833868
ASSAULT11 <77090569+ASSAULT11@users.noreply.github.com>
2021-02-15 12:17:44 +0900

    テストコード完成＋priceの値段制限バリデーション追加

--- app/models/furima.rb
+++ app/models/furima.rb
@@ -9,17 +9,21 @@ class Furima < ApplicationRecord
   belongs_to :area
   belongs_to :send_day
 
-
+  
   with_options presence: true do
     validates :image
     validates :item
     validates :text
-    validates :price
+    validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
   end
 
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :state_id,    numericality: { other_than: 1 } 
-  validates :shipping_id, numericality: { other_than: 1 } 
-  validates :area_id,     numericality: { other_than: 1 } 
-  validates :send_day_id, numericality: { other_than: 1 } 
+  with_options numericality: { other_than: 1 }  do
+    validates :category_id
+    validates :state_id
+    validates :shipping_id
+    validates :area_id
+    validates :send_day_id
+  end
+
+  validates :price, numericality: true
 end
