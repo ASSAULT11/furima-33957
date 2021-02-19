beb3aa23305b9408f66fde48817f70f7f919d128
ASSAULT11 <77090569+ASSAULT11@users.noreply.github.com>
2021-02-19 12:41:55 +0900

    formオブジェクトモデル記述

--- app/models/purchase_buy.rb
+++ app/models/purchase_buy.rb
@@ -1,3 +1,18 @@
 class PurchaseBuy < ApplicationRecord
-  
+  include ActiveModel::Model
+  attr_accessor :furima_id, :user_id, :postal_code, :prefecture, :city, :address, :building, :phone
+
+  with_options presence: true do
+    validates :postal_code
+    validates :city
+    validates :address
+    validates :phone
+  end
+
+  validates :prefectures_id, numericality: { other_than: 1 }
+
+  def save
+    purchase = Purchase.create(furima: furima_id, user_id: user_id)
+    Buy.create(postal_code: postal_code, prefecture: prefecture,city: city, address: address, building: building, phone: phone,purchase_id: purchase.id)
+  end
 end
