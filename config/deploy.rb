bd6f87d3dca4c2b99e95ddb9d024176e473fecc1
ASSAULT11 <77090569+ASSAULT11@users.noreply.github.com>
2021-02-25 18:53:31 +0900

    自動デプロイ設定完了

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+lock '3.15.0'
+
+set :application, 'furima-33957'
+
+set :repo_url,  'git@github.com:ASSAULT11/furima-33957.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5'
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/ryu123.pem'] 
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
